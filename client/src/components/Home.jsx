b914719d691f73d9d29a6537c5acafe65929ab7e
Kate <hello.kategrant@gmail.com>
2021-03-04 22:57:33 -0500

    added eyecandy div with liquid chrome img

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,9 +6,15 @@ import '../components/custom.css'
 
 
 
+
 function Home() {
     return (
         <div className="home">
+            <div className="eyeCandy">
+                <div className="corner">
+                    <img src={require("../components/knotimg8.png")} style={{width: "400px"}}/>
+                </div>
+            </div>
             <div className="adviceSection" style={{padding: "80px 0 0 50vw"}}>
                 <MyBox />
             </div>

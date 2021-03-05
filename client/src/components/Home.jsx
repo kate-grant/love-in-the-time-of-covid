78c62b01b1814b1bee114f93f07c7371e18ec0cc
Kate <hello.kategrant@gmail.com>
2021-03-05 10:44:34 -0500

    added scrolling text under navbar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,12 +10,15 @@ import '../components/custom.css'
 function Home() {
     return (
         <div className="home">
+            <div className="scrollerText">
+            ｡･ﾟﾟ･ *✧･ﾟ♥༻✧° ♡゜・.･༓･*♥˚⁺‧͙♡ 𐐪𐑂 °♡°ₒ♥̩̥̩♥̩̩̥͙♥̩͙ˊˎ
+            </div>
             <div className="eyeCandy">
                 <div className="corner">
                     <img src={require("../components/knotimg8.png")} style={{width: "400px"}}/>
                 </div>
             </div>
-            <div className="adviceSection" style={{padding: "80px 0 0 50vw"}}>
+            <div className="adviceSection" style={{padding: "30px 0 0 50vw"}}>
                 <MyBox />
             </div>
         </div>
